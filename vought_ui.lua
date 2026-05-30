c8622af02451f175c9c883c61ed969644cb506d5
PEDROWELES <ruslan995900@gmail.com>
2026-05-30 12:58:00 +0300

    Update vought_ui.lua

--- vought_ui.lua
+++ vought_ui.lua
@@ -2,7 +2,6 @@
 --@author User & Gemini
 --@client
 
-local fontMontserrat50 = render.createFont("Montserrat", 50, 500, true, false, false, false, 0, true, 0)
 local fontMontserrat24 = render.createFont("Montserrat", 24, 500, true, false, false, false, 0, true, 0)
 
 local notifyText = ""
@@ -10,7 +9,7 @@ local notifySubText = ""
 local notifyAlpha = 0
 local notifyEndTime = 0
 
--- Получение уведомления от сервера
+-- Получение сетевого сигнала от сервера
 net.receive("vought_ui_notify", function()
     notifyText = net.readString()
     notifySubText = net.readString()
@@ -18,10 +17,11 @@ net.receive("vought_ui_notify", function()
     notifyAlpha = 0
 end)
 
-hook.add("drawCellular", "VoughtNotification", function()
+-- ИСПРАВЛЕНО: Теперь хук HUDPaint, чтобы рисовалось прямо на экране игрока, а не на чипе
+hook.add("HUDPaint", "VoughtNotification", function()
     if timer.curtime() > notifyEndTime and notifyAlpha <= 0 then return end
     
-    -- Плавное появление и исчезновение
+    -- Плавное появление и исчезновение плашки
     if timer.curtime() < notifyEndTime then
         notifyAlpha = math.Approach(notifyAlpha, 255, render.getFrameTime() * 500)
     else
@@ -29,29 +29,31 @@ hook.add("drawCellular", "VoughtNotification", function()
     end
     
     local scrW, scrH = render.getResolution()
-    local w, h = 500, 80
+    local w, h = 600, 80
     local x = (scrW - w) / 2
-    local y = scrH - h - 60 -- Позиция снизу экрана
+    local y = scrH - h - 100 -- Позиция снизу экрана, чуть выше стандартного HUD
     
-    -- Задний фон уведомления
-    render.setColor(Color(20, 20, 20, notifyAlpha * 0.85))
-    render.drawRect(x, y, w, h)
-    
-    -- Левая светящаяся полоска (эффект Vought)
-    render.setColor(Color(180, 255, 0, notifyAlpha))
-    render.drawRect(x, y, 6, h)
-    
-    -- Окантовка
-    render.setColor(Color(255, 255, 255, notifyAlpha * 0.1))
-    render.drawRectOutline(x, y, w, h, 1)
-    
-    -- Текст заголовка
-    render.setFont(fontMontserrat24)
-    render.setColor(Color(180, 255, 0, notifyAlpha))
-    render.drawText(x + 20, y + 15, notifyText, 0)
-    
-    -- Текст описания управления
-    render.setFont(fontMontserrat24)
-    render.setColor(Color(255, 255, 255, notifyAlpha * 0.9))
-    render.drawText(x + 20, y + 45, notifySubText, 0)
+    -- Рисуем в 2D контексте экрана
+    render.start2D()
+        -- Задний фон (темный)
+        render.setColor(Color(20, 20, 20, notifyAlpha * 0.85))
+        render.drawRect(x, y, w, h)
+        
+        -- Левая неоновая полоска
+        render.setColor(Color(180, 255, 0, notifyAlpha))
+        render.drawRect(x, y, 6, h)
+        
+        -- Рамка
+        render.setColor(Color(255, 255, 255, notifyAlpha * 0.1))
+        render.drawRectOutline(x, y, w, h, 1)
+        
+        -- Текст заголовка
+        render.setFont(fontMontserrat24)
+        render.setColor(Color(180, 255, 0, notifyAlpha))
+        render.drawText(x + 20, y + 15, notifyText, 0)
+        
+        -- Текст управления (на русском)
+        render.setColor(Color(255, 255, 255, notifyAlpha * 0.9))
+        render.drawText(x + 20, y + 45, notifySubText, 0)
+    render.end2D()
 end)
