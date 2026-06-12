1953d7f51b830539baca0a17a284bb71c75db3b0
PEDROWELES <ruslan995900@gmail.com>
2026-06-12 13:39:56 +0300

    Update speedsound.lua

---
+++ speedsound.lua
@@ -0,0 +1,138 @@
+---@name Sounds
+---@author vertihluy
+---@shared
+
+---@class astrosounds
+local astrosounds = {}
+
+-- =========================================================================
+-- НАСТРОЙКА ЗВУКОВ (Конфиг прямо тут, на гитхабе)
+-- =========================================================================
+local SOUND_CONFIG = {
+    ["superspeed_run"] = {
+        url = "https://www.image2url.com/r2/default/audio/1781259648449-8613a672-c1aa-4985-8dd9-91aa74952114.mp3",
+        volume = 1,
+        loop = true
+    }
+    -- Сюда в будущем ты сможешь дописывать новые звуки через запятую:
+    -- ["имя"] = { url = "...", volume = 0.8, loop = false }
+}
+-- =========================================================================
+
+if SERVER then
+    ---Play sound
+    ---@param name string Identifier of sound
+    ---@param offset Vector? Position or offset of this sound
+    ---@param parent Entity? Entity, parent this sound to
+    ---@param plys table | Player | nil Players to send the sound
+    function astrosounds.play(name, offset, parent, plys)
+        net.start("playSound")
+        net.writeString(name)
+        net.writeVector(offset or Vector())
+        net.writeBool(parent ~= nil)
+        if parent then
+            net.writeEntity(parent)
+        end
+        net.send(plys)
+    end
+
+    ---Stop sound
+    ---@param name string Identifier of sound
+    ---@param plys table | Player | nil Players to stop the sound
+    function astrosounds.stop(name, plys)
+        net.start("stopSound")
+        net.writeString(name)
+        net.send(plys)
+    end
+
+else
+    local SOUNDS = {}
+    local PARENTS = {}
+    local ERRORS = {}
+
+    local function message(...)
+        printConsole(Color(255, 0, 0), "[AstroSound] ", Color(255, 255, 255), ...)
+    end
+
+    ---Preload sound
+    function astrosounds.preload(name, volume, loop, play, url)
+        local noplay = (!play and " noplay" or "")
+        bass.loadURL(url, "3d noblock" .. noplay, function(snd, _, errname)
+            if !snd then
+                local attempts = ERRORS[name] or 3
+                message(string.format("Sound \"%s\" error: %s. Attempts remain: %i", name, errname, attempts))
+
+                if attempts and attempts > 0 then
+                    ERRORS[name] = attempts - 1
+                    timer.simple(1, function()
+                        astrosounds.preload(name, volume, loop, play, url)
+                    end)
+                end
+                return
+            end
+ 
+            SOUNDS[name] = snd
+            message(string.format("Sound \"%s\" loaded!", name))
+            snd:setVolume(volume)
+            snd:setLooping(loop)
+        end)
+    end
+
+    ---Play sound
+    function astrosounds.play(name, offset, parent)
+        offset = offset or Vector()
+        local sound = SOUNDS[name]
+        if sound then
+            if !sound:isLooping() then
+                sound:setTime(0)
+            end
+            local parentPos = (parent and parent:getPos() or Vector())
+            sound:setPos(parentPos + offset)
+            sound:play()
+            PARENTS[name] = {parent, offset}
+        end
+    end
+
+    ---Stop sound
+    function astrosounds.stop(name)
+        local sound = SOUNDS[name]
+        if sound then
+            sound:pause()
+            sound:setTime(0)
+        end
+    end
+
+    -- Автоматический прелоад всех звуков из таблицы конфигурации при запуске на клиенте
+    for soundName, config in pairs(SOUND_CONFIG) do
+        astrosounds.preload(soundName, config.volume, config.loop, false, config.url)
+    end
+
+    net.receive("playSound", function()
+        local name = net.readString()
+        local pos = net.readVector()
+        local is_parent = net.readBool()
+        if is_parent then
+            net.readEntity(function(ent)
+                astrosounds.play(name, pos, ent)
+            end)
+        else
+            astrosounds.play(name, pos)
+        end
+    end)
+
+    net.receive("stopSound", function()
+        local name = net.readString()
+        astrosounds.stop(name)
+    end)
+
+    hook.add("Think", "soundParent", function()
+        for name, parent in pairs(PARENTS) do
+            local snd = SOUNDS[name]
+            if snd and isValid(parent[1]) then
+                snd:setPos(parent[1]:getPos() + parent[2])
+            end
+        end
+    end)
+end
+
+return astrosounds
